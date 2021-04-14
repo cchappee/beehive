720562bba4060659452e0a91d41af06f88582996
cchappee <cchapp3@uic.edu>
2021-04-14 17:57:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import os
+import time
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-client = WebClient(token='Bot_User_OAuth_Token')
 
-try:
-    filepath="./TemperatureData.csv"
-    response = client.files_upload(channels='#updates', file=filepath)
-    assert response["file"]  # the uploaded file
-except SlackApiError as e:
-    # You will get a SlackApiError if "ok" is False
-    assert e.response["ok"] is False
-    assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
-    print(f"Got an error: {e.response['error']}")
+while True:
+    try:
+        filepath="./TemperatureData.csv"
+        response = client.files_upload(channels='#updates', file=filepath)
+        assert response["file"]  # the uploaded file
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["ok"] is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+        print(f"Got an error: {e.response['error']}")
+    time.sleep(3600)
