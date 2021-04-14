97c8f945a9d836510ffe9b5b91b358dc9efeef56
cchappee <cchapp3@uic.edu>
2021-04-14 17:33:30 -0500

    the token isnt included

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-client = WebClient(token='xoxb-1868163589349-1924651479234-OFy4C8ciJbXgvcK9wHQdrZ8w')
+client = WebClient(token='Bot_User_OAuth_Token')
 
 try:
-    response = client.chat_postMessage(channel='#updates', text="Save the Bees!")
-    assert response["message"]["text"] == "Save the Bees!"
+    filepath="./TemperatureData.csv"
+    response = client.files_upload(channels='#updates', file=filepath)
+    assert response["file"]  # the uploaded file
 except SlackApiError as e:
+    # You will get a SlackApiError if "ok" is False
     assert e.response["ok"] is False
-    assert e.response["error"]
+    assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
     print(f"Got an error: {e.response['error']}")
